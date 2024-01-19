e3af3e1297e1fcc7440b5f6dc777a19b8136447d
muhammad-ahsan099 <taseries260@gmail.com>
2024-01-20 01:34:51 +0500

    add email feature

--- src/api/invitation/content-types/invitation/lifecycles.js
+++ src/api/invitation/content-types/invitation/lifecycles.js
@@ -1,9 +1,19 @@
 module.exports = {
   async afterCreate(event) {
     const {
-      result: { id, invitee_email, invitee_name, createdBy },
+      result: { id, inviter, invitee_email, invitee_name, createdBy },
     } = event;
     console.log("Event++: ", event.result);
+
+    const user = await strapi.db
+      .query("plugin::users-permissions.user")
+      .findOne({
+        where: {  id: parseInt(inviter) },
+      });
+
+    if (!user) {
+      return
+    }
 
     try {
       // only send the email if the email is not a job_rejection mail
@@ -12,7 +22,7 @@ module.exports = {
         from: "SetId <devsetid@gmail.com>",
         subject: "SetId Mediation Invitation",
         html: `<p>Hello ${invitee_name},</p>
-               <p>${createdBy?.firstname} ${createdBy?.lastname} has invited you to SetId Mediation. Click the link below to get started:</p>
+               <p>${user?.firstname} ${user?.lastname} has invited you to SetId Mediation. Click the link below to get started:</p>
                <p><a href="http://localhost:3000/get-started?invited_by=${encodeURIComponent(createdBy?.firstname + ' ' + createdBy?.lastname)}&id=${createdBy?.id}&invitation_id=${id}">http://localhost:3000/get-started?invited_by=${encodeURIComponent(createdBy?.firstname + ' ' + createdBy?.lastname)}&id=${createdBy?.id}&invitation_id=${id}</a></p>`
       });      
 
