49d6452f920e6d5f1a355c78a5c80577880498c5
muhammad-ahsan099 <taseries260@gmail.com>
2024-01-20 01:04:28 +0500

    add email feature

---
+++ src/api/invitation/content-types/invitation/lifecycles.js
@@ -0,0 +1,23 @@
+module.exports = {
+  async afterCreate(event) {
+    const {
+      result: { id, invitee_email, invitee_name, createdBy },
+    } = event;
+    console.log("Event++: ", event.result);
+
+    try {
+      // only send the email if the email is not a job_rejection mail
+      await strapi.plugins["email"].services.email.send({
+        to: invitee_email,
+        from: "SetId <devsetid@gmail.com>",
+        subject: "SetId Mediation Invitation",
+        html: `<p>Hello ${invitee_name},</p>
+               <p>${createdBy?.firstname} ${createdBy?.lastname} has invited you to SetId Mediation. Click the link below to get started:</p>
+               <p><a href="http://localhost:3000/get-started?invited_by=${encodeURIComponent(createdBy?.firstname + ' ' + createdBy?.lastname)}&id=${createdBy?.id}&invitation_id=${id}">http://localhost:3000/get-started?invited_by=${encodeURIComponent(createdBy?.firstname + ' ' + createdBy?.lastname)}&id=${createdBy?.id}&invitation_id=${id}</a></p>`
+      });      
+
+    } catch (e) {
+      console.log("EMAIL SENDING ERROR: ", e);
+    }
+  },
+};
